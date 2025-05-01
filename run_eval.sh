3fa3a0f300839fc5de3c233c562ab23bc777d648
Jazsmin Gilliam <jazzlynetgeeter@gmail.com>
2025-05-01 01:08:21 -0500

    added shell script to run evaluation on popularity model

---
+++ run_eval.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=evaluation
+#SBATCH --output=bert_output_%j.log
+#SBATCH --error=bert_error_%j.log
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH --time=01:00:00
+#SBATCH --partition=bigTiger
+
+
+conda activate multimodal-popularity
+
+cd /project/jlglliam/final_project
+
+python eval_and_stat_graphs.py
