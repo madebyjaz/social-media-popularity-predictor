bb968bb20e9df3aef76e3706ca8a6e53ab78b110
Jazsmin Gilliam <jazzlynetgeeter@gmail.com>
2025-05-02 15:56:44 -0500

    added the shell to run the eval job

--- run_eval.sh
+++ run_eval.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=evaluation
+#SBATCH --job-name=eval
 #SBATCH --output=bert_output_%j.log
 #SBATCH --error=bert_error_%j.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
-#SBATCH --time=01:00:00
+#SBATCH --time=04:00:00
 #SBATCH --partition=bigTiger
 
 
